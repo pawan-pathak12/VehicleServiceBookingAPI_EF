75acddb83be2f44ec745bd79d8c16fa872a16a50
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-09-15 20:51:59 +0545

    refactor: separate entity mappings into individual profile classes

---
+++ Mapper/CustomerProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using VehicleServiceBookingAPI_EF.DTOs.Customers;
+using VehicleServiceBookingAPI_EF.Entity;
+
+namespace VehicleServiceBookingAPI_EF.Mapper
+{
+    public class CustomerProfile:Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, ResponseCustomerDTO>();
+            CreateMap<CreateCustomerDTO, Customer>();
+            CreateMap<UpdateCustomerDTO, Customer>();
+            CreateMap<PatchCustomerDTO, Customer>();
+        }
+    }
+}
---
+++ Mapper/ServiceTypeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System.CodeDom;
+using VehicleServiceBookingAPI_EF.DTOs.ServiceTypes;
+using VehicleServiceBookingAPI_EF.Entity;
+
+namespace VehicleServiceBookingAPI_EF.Mapper
+{
+    public class ServiceTypeProfile:Profile
+    {
+        public ServiceTypeProfile()
+        {
+
+            CreateMap<ServiceType, ResponseServiceTypeDTO>();
+            CreateMap<CreateServiceTypeDTO, ServiceType>();
+            CreateMap<UpdateServiceTypeDTO, ServiceType>();
+        }
+    }
+}
